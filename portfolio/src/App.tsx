db1653d19370913a1ca7f27d7ee525c34591fc38
Ruben Bromée <ruben.bromee@gmail.com>
2023-01-12 16:25:26 +0100

    modal

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@chakra-ui/button";
-import { Text, Flex, Link, SimpleGrid, Image } from "@chakra-ui/react";
+import { Text, Flex, Link, SimpleGrid, Image, Heading } from "@chakra-ui/react";
 import React, { useState } from "react";
 import projects from "./assets/projects.json";
+import ProjectModal, { Project } from "./ProjectModal";
 
 const App: React.FC = () => {
+	const [openedProject, setOpenedProject] = useState<Project>();
+	const onClose = () => {
+		setOpenedProject(undefined);
+	};
+
 	const renderProjects = () => {
 		let renders: JSX.Element[] = [];
 		projects.data.map((project) => {
@@ -18,9 +24,11 @@ const App: React.FC = () => {
 					border={"1px solid #D3D3D3"}
 					boxShadow={"5px 5px 3px 0px rgba(212,212,212,0.75)"}
 					padding={"10px"}
+					onClick={() => setOpenedProject(project)}
+					cursor={"pointer"}
 				>
 					<Flex maxW={"225px"} textAlign={"center"}>
-						<Text size={"lg"} marginY={"10px"}>
+						<Text fontSize={"lg"} marginY={"10px"}>
 							{project.name}
 						</Text>
 					</Flex>
@@ -46,21 +54,46 @@ const App: React.FC = () => {
 			alignItems={"center"}
 			flexDirection={"column"}
 		>
-			<Text>Portfolio page of Ruben Bromée</Text>
-			<Flex gap={"10px"}>
-				<Link href={"mailto:ruben.bromee@gmail.com"}>
+			<Heading size={"2xl"} marginTop={"25px"}>
+				Portfolio page of Ruben Bromée
+			</Heading>
+			<Flex gap={"10px"} marginY={"25px"} alignItems={"center"}>
+				<Link href={"mailto:ruben.bromee@gmail.com"} fontSize={"lg"}>
 					ruben.bromee@gmail.com
 				</Link>
-				<Link href={"https://www.linkedin.com/in/ruben-brom%C3%A9e/"}>
+				<Link
+					onClick={() => {
+						window.open(
+							"https://www.linkedin.com/in/ruben-brom%C3%A9e/"
+						);
+					}}
+					fontSize={"lg"}
+				>
 					LinkedIn
 				</Link>
-				<Link href={"https://github.com/Rubenbromee"}>GitHub</Link>
+				<Link
+					onClick={() =>
+						window.open("https://github.com/Rubenbromee")
+					}
+					fontSize={"lg"}
+				>
+					GitHub
+				</Link>
+				<Button colorScheme={"blue"}>Get CV</Button>
 			</Flex>
-			<Button>CV</Button>
-			<Text>Projects</Text>
-			<SimpleGrid columns={3} gap={"20px"}>
-				{renderProjects()}
-			</SimpleGrid>
+			<Heading size={"lg"}>Projects</Heading>
+			<Flex marginY={"25px"}>
+				<SimpleGrid columns={3} gap={"20px"}>
+					{renderProjects()}
+				</SimpleGrid>
+			</Flex>
+			{openedProject != undefined && (
+				<ProjectModal
+					project={openedProject}
+					open={openedProject != undefined}
+					onClose={onClose}
+				/>
+			)}
 		</Flex>
 	);
 };
---
+++ portfolio/src/ProjectModal.tsx
@@ -0,0 +1,56 @@
+import {
+	Button,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	Text,
+} from "@chakra-ui/react";
+
+export type Project = {
+	name: string;
+	description: string;
+	link: string;
+	image: string;
+};
+
+type Props = {
+	project?: Project;
+	open: boolean;
+	onClose: () => void;
+};
+
+const ProjectModal: React.FC<Props> = ({ project, open, onClose }: Props) => {
+	const openLink = () => {
+		window.open(project?.link);
+	};
+
+	return (
+		<>
+			{project && (
+				<Modal isOpen={open} onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent padding={"25px"}>
+						<ModalHeader>{project.name}</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<Text>{project.description}</Text>
+							<Button
+								colorScheme={"blue"}
+								marginY={"15px"}
+								onClick={openLink}
+							>
+								Project
+							</Button>
+						</ModalBody>
+					</ModalContent>
+				</Modal>
+			)}
+		</>
+	);
+};
+
+export default ProjectModal;
